26d6f99fa01d35c14b459245048cbebb450163d3
devgl96 <georgelucas.dev@gmail.com>
2024-07-10 10:47:23 -0300

    Lesson 02 completed

---
+++ src/pages/trip-details/important-links.tsx
@@ -0,0 +1,42 @@
+import { Link2, Plus } from "lucide-react";
+import { Button } from "../../components/button";
+
+interface ImportantLinksProps {}
+
+export function ImportantLinks() {
+  return (
+    <div className="space-y-6">
+      <h2 className="font-semibold text-xl">Links importantes</h2>
+
+      <div className="space-y-5">
+        <div className="flex items-center justify-between gap-4">
+          <div className="space-y-1.5">
+            <span className="block font-medium text-zinc-100">
+              Reserva do AirBnB
+            </span>
+            <a className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+              https://www.airbnb.com.br/rooms/10470001115273974395268762983764928
+            </a>
+          </div>
+          <Link2 className="text-zinc-400 size-5 shrink-0" />
+        </div>
+        <div className="flex items-center justify-between gap-4">
+          <div className="space-y-1.5">
+            <span className="block font-medium text-zinc-100">
+              Reserva do AirBnB
+            </span>
+            <a className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+              https://www.airbnb.com.br/rooms/10470001115273974395268762983764928
+            </a>
+          </div>
+          <Link2 className="text-zinc-400 size-5 shrink-0" />
+        </div>
+      </div>
+
+      <Button variant="secondary" size="full">
+        <Plus className="size-5" />
+        Cadastrar novo link
+      </Button>
+    </div>
+  );
+}
